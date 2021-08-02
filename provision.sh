baa509220d153b5e0e12466d80af41a30f6c4cfc
saim22r <saimrafiq224@gmail.com>
2021-08-02 16:51:46 +0100

    Jenkins task

---
+++ provision.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt install openjdk-11-jdk -y
+java -version
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+    /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update -y
+sudo apt-get install jenkins -y
