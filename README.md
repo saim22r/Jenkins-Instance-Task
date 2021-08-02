3d97b7e0bdcb9ee7407153a92db78d371fd14076
saim22r <saimrafiq224@gmail.com>
2021-08-02 17:22:36 +0100

    Update

---
+++ README.md
@@ -0,0 +1,5 @@
+# Running Jenkins Locally
+
+## Prerequisites 
+
+## Vagrantfile and provision
--- provision.sh
+++ provision.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt install openjdk-11-jdk -y
+
+sudo apt-get update -y # Update package list
+sudo apt-get upgrade -y # Find new versions of the packages
+
+# Install the latest version of java
+sudo apt install openjdk-11-jdk -y 
 java -version
+
+# Install jenkins
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
     /etc/apt/sources.list.d/jenkins.list'
